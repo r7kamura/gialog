1d6f872a1e37d5d6cc57c1ac4322bc40bb89bd4a
Ryo Nakamura <r7kamura@gmail.com>
2022-05-02 06:40:20 +0900

    Fix error on project site (non user site)

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: process.env.GITHUB_REPOSITORY
+    ? `/${process.env.GITHUB_REPOSITORY.split("/")[1]}`
+    : "",
   reactStrictMode: true,
 };
 
