6655ddf179dd9cdde179dcbc883854721aad0cb2
Ryo Nakamura <r7kamura@gmail.com>
2022-05-01 19:49:35 +0900

    Add article page and improve UI

---
+++ components/Layout.js
@@ -0,0 +1,35 @@
+import Head from "next/head";
+import Link from "next/link";
+
+export default function Layout({ children }) {
+  return (
+    <>
+      <header>
+        <nav>
+          <p>
+            <Link href="/">
+              <a>github-issues-as-blog</a>
+            </Link>
+          </p>
+        </nav>
+      </header>
+      <main>{children}</main>
+      <footer>
+        <nav>
+          <ul>
+            <li>
+              <Link href="/">
+                <a>Home</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/about">
+                <a>About</a>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </footer>
+    </>
+  );
+}
---
+++ pages/articles/[issueNumber].tsx
@@ -0,0 +1,51 @@
+import type { NextPage } from "next";
+import { getIssue, listIssues } from "../../lib/issue";
+
+type Props = {
+  issue: Issue;
+};
+
+type Issue = any;
+
+const ShowArticle: NextPage<Props> = ({ issue }) => {
+  console.log(issue);
+
+  return (
+    <article>
+      <header>
+        <h1>{issue.title}</h1>
+      </header>
+      <div>
+        {issue.body}
+      </div>
+    </article>
+  );
+};
+
+export default ShowArticle;
+
+export async function getStaticPaths() {
+  const paths = listIssues().map((issue) => {
+    return {
+      params: {
+        issueNumber: issue.number.toString(),
+      },
+    };
+  });
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }: any) {
+  const issueNumber = parseInt(params.issueNumber, 10);
+
+  const issue = getIssue({ issueNumber });
+  console.log(issueNumber);
+  return {
+    props: {
+      issue,
+    },
+  };
+}
