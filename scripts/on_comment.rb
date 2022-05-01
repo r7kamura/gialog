214548636fa1af97856715f1181ca15afea5f732
Ryo Nakamura <r7kamura@gmail.com>
2022-05-01 18:13:39 +0900

    Write issue data to issues.json on issue event

--- scripts/on_comment.rb
+++ scripts/on_comment.rb
@@ -33,6 +33,72 @@ class GetGitHubPayload
   end
 end
 
+class Database
+  DEFAULT_VALUE = {}
+
+  class << self
+    # @return [Hash]
+    def read
+      new.read
+    end
+
+    # @param [Hash] data
+    def write(data)
+      new.write(data)
+    end
+  end
+
+  # @return [Hash]
+  def read
+    if pathname.exist?
+      ::JSON.parse(content)
+    else
+      DEFAULT_VALUE
+    end
+  end
+
+  # @param [Hash] data
+  def write(data)
+    pathname.write(
+      ::JSON.dump(data)
+    )
+  end
+
+  private
+
+  # @return [Pathname]
+  def pathname
+    ::Pathname.new(path)
+  end
+
+  # @return [String]
+  def path
+    'issues.json'
+  end
+end
+
+class UpdateIssue
+  class << self
+    # @param [Hash] issue
+    def call(issue)
+      new(issue).call
+    end
+  end
+
+  # @param [Hash] issue
+  def initialize(issue)
+    @issue = issue
+  end
+
+  def call
+    data = ::Database.read
+    data['issues'][@issue['id']] = @issue
+    ::Database.write(data)
+  end
+end
+
 payload = GetGitHubPayload.call
 
 pp payload
+
+UpdateIssue.call(payload['issue'])
